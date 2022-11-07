aba05d23c94b31812761ac4fb90dcf09c7d67326
Maxim Polyakov <maxpol1997@gmail.com>
2022-11-07 23:57:09 +0400

    task-01 added console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ import { HttpService } from './services/http.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  constructor() {
+    console.log("Task 01 done");
+  }
 }
 
 
